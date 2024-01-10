cd4df3e6b5cde38a42d32dad43168bf84c93e0e1
Dennis marakhovski <dheghmanns@yahoo.de>
2024-01-10 22:00:30 +0100

    Angefangen mit der Dokumentation für py und js

---
+++ js.boilerplate/javascript.js
@@ -0,0 +1,25 @@
+
+let password  //erschafft die variable password.
+password = "Hello";   // Speichert Hello in der variable password.
+
+//--------------------------         Verschiedene arten mehrere Variablen zu erstellen, alter befehl für let war "var"
+
+let Name = "Dennis";   //Zeile 2 und 4 sind auch so kombienier bar.
+
+let Alter = 18, Ort = "Unna", Sprache = "Russisch"; // mit einem let befehl kann man mehrer Variablen erschaffen, zahlen brauchen keine "".
+
+let Alter = 18;
+let Ort = "Unna";
+let Sprache = "Russisch"
+
+let Alter = 18,
+    Ort = "Unna",
+    Sprache = "Russisch";
+
+let Alter = 18
+   ,Ort = "Unna" 
+   ,Sprache = "Russisch";
+
+//--------------------------------
+
+alert(message); //zeigt was in der Variable Liegt.
---
+++ py.boilerplate/main.py
@@ -0,0 +1,36 @@
+
+# input("Wie heist du? ") #fordert vom benutzen den input "wie heist du an"
+name = input("Wie heist du? ")  #setzt die antwort auf wie  heist du der variable name gleich 
+#print(name)        #print gibt das argument im terminal aus, in dem fall die variable name
+
+stadt = input("Wo kommst du her? ")
+hobby = input("was sind deine hobys? ")
+alter = input("Wie alt bist du?" )
+lohn_stunde = input("wieviel verdienst du pro stunde? ")
+
+
+
+print("Hallo ich heiße "+name+" und ich komme aus "+stadt+". Mein hobby ist "+hobby+" und ich bin "+alter+" jahre alt.")
+
+name = True        #deklatiert name als true
+
+if name:
+    print("kaiser")                # if else schleife, wenn der wert bei if true ist wird,
+else:                               #  kaiser aus gegebn "sonst (else)" wird Hallo {name} variable ausgegebe.
+   print(f"Hallo {name}")             # true or false sind boolische werte, boolisch bedeutet ein wert 1 oder 0 wobei true=1 fals=0
+
+input("Bitte beliebige Taste drücken... ")
+
+#str = text
+#int = Ganze zahl zb 1 2 3 4 ...
+#float = comma zahl. 1,2  4,6   1,999
+#bool = lässt einer variable den wert fals or true zuweisen
+
+# +   = bei strings nicht wie in der normalen mathematim verbindet es hier die strings und rechnet sie nicht zusammen, 1 +1 = 11. das + verbindet die strings.
+# = setzt gleich mit,  also name = input   setzt die variable name indem fall mit dem input des benutzers gleich.
+# , = innerhalb von argumenten trennt es sie und sagt dem program das sie in einem schrit ausgeführt werden sollen fügt auserdem im return ein lehrzeichen zwischen beide werte.
+#"24" = zahlen in anführungszeichen betonnen das es sich um strings handelt-
+
+
+
+
