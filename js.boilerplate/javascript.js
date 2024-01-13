5c31bff0d34b51472ee4eb23595848852a10a6c5
Dennis marakhovski <dheghmanns@yahoo.de>
2024-01-13 10:51:16 +0100

    Überarbeitung und aufräumen, Mathe grundlagen in javascript

--- js.boilerplate/javascript.js
+++ js.boilerplate/javascript.js
@@ -1,25 +1,83 @@
 
-let password  //erschafft die variable password.
+let password, Name; //erschafft die variable password und Name var ist de alte command dafür.
 password = "Hello";   // Speichert Hello in der variable password.
+Name = "Dennis";     // Speichert Dennis in der Variable Name.
+password = Name;    // Ändert den inhalt der Variable Name auf den Inhalt der Variable password.
+const Birthday = "16.12.2005";   //erschafft eine unänderliche constante
+//typeof // zeigt den jeweiligen datentyp an der variable
 
-//--------------------------         Verschiedene arten mehrere Variablen zu erstellen, alter befehl für let war "var"
 
-let Name = "Dennis";   //Zeile 2 und 4 sind auch so kombienier bar.
+const longcomma = 1.534464562434634526;
+const pointonecomma = longcomma.toFixed(1);   // .toFixed() rundet die comma zahl auf die gewünschte nach komma stelle.
+let myNumber = "74";     // "  " gibt den input 74 als datatyp string nicht als number
+myNumber += 3; // wie bei python werden die strings zusamen addiert und es kommt 743 raus.
+myNumber = Number(myNumber) + 3; // Wandelt den String "74" in einen datatyp number
+console.log(myNumber)
 
-let Alter = 18, Ort = "Unna", Sprache = "Russisch"; // mit einem let befehl kann man mehrer Variablen erschaffen, zahlen brauchen keine "".
-
-let Alter = 18;
-let Ort = "Unna";
-let Sprache = "Russisch"
-
-let Alter = 18,
-    Ort = "Unna",
-    Sprache = "Russisch";
-
-let Alter = 18
-   ,Ort = "Unna" 
-   ,Sprache = "Russisch";
 
 //--------------------------------
 
-alert(message); //zeigt was in der Variable Liegt.
+alert(password); //zeigt was in der Variable Liegt als seperate nachricht oben im Fenster.
+console.log()   // zeigt den content im Browser log an
+
+//-------------------------------- Operatoren
+// "unary", der operator ist unary wenn wenn nur ein Operand benutz wird, wenn zwei operanten vor kommen dann ist die rechnung binary.
+
+
+// - Addition
+// - Subtraction
+// + Multiplication
+// ** Exponentation
+// / Division
+// % Modulus
+// ++ increment
+// --  Decrement
+
+// Arithemische operationen:
+
+let x = 100 + 50,    // 100 ist ein Operand, + ist der operator, und 50 wieder ein Operand.
+    a = 2,
+    b = x * a;
+
+
+let n = 5,
+    z = x ** 2;   // x ** y macht das gleiche wie  Math.pow(x,2);
+
+    console.log(z)
+
+    // verschiedene zahlen typen
+
+    int     // ganze zahlt ohne komma und kein Bruch
+    float   // comma zahlen 
+    Doubles  // Genauere floats in den hinteren nach comma stellen
+
+    Decimal // 1-9 in jeder stelle
+    Binary  // einsen und nullen
+    Octal   // basis ist 8, benutz in jeders telle 0-7
+    Hexadecimal // basis ist 16, benutzt 0-9 und a-f, bsp Hexa farben code.
+
+    floats, int // beides In javascript "Number"  Biglnt noch für sehr sehr große int
+
+    Boolean() // flasch oder wahr
+
+    // vergleichs operatoren 
+
+    === strict equality // fragt ob beide werte identisch sind
+    !== Strict-non-equality // Fragt ob beide werde nicht identisch sind
+    < //Less than // testet ob der linke wert kleiner ist als der rechte
+    > Greater then // testet ob der linke wert größer ist als der rechte.
+    <== lesser then or equal // testet ob der linke wert kleiner ist oder gleich
+    >== greater then or equal // testet ob der linke wert größer oder gleich ist
+
+    // Aufgaben operatoren 
+
+    += Addition assigment // gibt den linken wert zu dem rechten wert und gibt dann denn neuen wert raus 
+    -= Subtraction assigment // subtrahiert den wert link vom wert rechts und gibt denn nneuen wert aus 
+    *= Multiplication assigment // multipliziert den wert links mit dem wert rechts und gibt denn neuen wert aus 
+    /= Division assigment // teilt den linken wert mit dem rechten wert und gibt den neuen wert aus
+   // der operator kann vor oder nach der Nummer stehen, der unterschied ist ob er bei x *= 4 zu erst ausgibt und dann umrechnet oder bei *= 4 zuerst umrechnet dann ausgibt.
+
+
+    
+
+
--- py.boilerplate/main.py
+++ py.boilerplate/main.py
@@ -6,9 +6,6 @@ name = input("Wie heist du? ")  #setzt die antwort auf wie  heist du der variabl
 stadt = input("Wo kommst du her? ")
 hobby = input("was sind deine hobys? ")
 alter = input("Wie alt bist du?" )
-lohn_stunde = input("wieviel verdienst du pro stunde? ")
-
-
 
 print("Hallo ich heiße "+name+" und ich komme aus "+stadt+". Mein hobby ist "+hobby+" und ich bin "+alter+" jahre alt.")
 
@@ -19,10 +16,9 @@ if name:
 else:                               #  kaiser aus gegebn "sonst (else)" wird Hallo {name} variable ausgegebe.
    print(f"Hallo {name}")             # true or false sind boolische werte, boolisch bedeutet ein wert 1 oder 0 wobei true=1 fals=0
 
-input("Bitte beliebige Taste drücken... ")
 
 #str = text
-#int = Ganze zahl zb 1 2 3 4 ...
+#int = Ganze zahlt zb 1 2 3 4 ...
 #float = comma zahl. 1,2  4,6   1,999
 #bool = lässt einer variable den wert fals or true zuweisen
 
@@ -30,7 +26,6 @@ input("Bitte beliebige Taste drücken... ")
 # = setzt gleich mit,  also name = input   setzt die variable name indem fall mit dem input des benutzers gleich.
 # , = innerhalb von argumenten trennt es sie und sagt dem program das sie in einem schrit ausgeführt werden sollen fügt auserdem im return ein lehrzeichen zwischen beide werte.
 #"24" = zahlen in anführungszeichen betonnen das es sich um strings handelt-
-
 
 
 
